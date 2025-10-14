7aafd2fd530f82c2a8e7b12ec12d3b2f6179ceff
gunlimpanit12345678-arch <gunlimpanit12345678@gmail.com>
2025-10-14 11:01:50 +0700

    at all assets

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charator : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int health;
+
+    public int Health
+    { get => health;
+        set => health = (value < 0) ? 0 : value; }
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Debug.Log($"{this.name} took damage {damage}. Current Health {Health}");
+    }
+
+    public bool IsDead()
+    {
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+            Debug.Log($"{this.name} is dead and destroy.");
+            return false;
+        }
+        else return false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
