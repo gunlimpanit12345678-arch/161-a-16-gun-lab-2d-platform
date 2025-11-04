125885c7358fd9567031126922051b157357ed37
gunlimpanit12345678-arch <gunlimpanit12345678@gmail.com>
2025-11-04 23:43:23 +0700

    HP bar

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -11,21 +11,27 @@ public class Charater : MonoBehaviour
     { get => health;
         set => health = (value < 0) ? 0 : value; }
 
+    private int maxHealth;
+
     protected Animator anim;
     protected Rigidbody2D rb;
+    [SerializeField] HealthBar healthBar;
 
     public void Initialize(int startHealth)
     {
         Health = startHealth;
+        maxHealth = startHealth;
         Debug.Log($"{this.name} initial Health : {this.health}.");
 
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        healthBar = GetComponentInChildren<HealthBar>();
     }
 
     public void TakeDamage(int damage)
     {
         Health -= damage;
+        healthBar.UpdateHealthBar(Health, maxHealth);
         Debug.Log($"{this.name} took damage {damage}. Current Health {Health}");
         IsDead();
     }
